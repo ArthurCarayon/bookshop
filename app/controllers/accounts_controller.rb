229dfeec264d409636572345c3e1f8be78fc9b26
ARTHURC <arthur.carayon@gmail.com>
2022-10-21 09:29:37 +0200

    Fix save user in accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,4 +3,19 @@ class AccountsController < ApplicationController
     def index
         @user = User.find(current_user.id) 
     end
+
+    def update
+        @user = User.find(current_user.id) 
+        if @user.update(users_params)
+            redirect_to(root_path)
+        else
+            redirect_to(root_path)
+        end
+    end
+
+    private
+
+        def users_params
+            params.require(:user).permit(:firstname, :lastname, :email)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   resources :books
 
-  get '/accounts/', to: 'accounts#index'
+  ## needhelp, i added :as => user, after "undefined method `user_path'" in accounts/form. So following this advise from SO:
+  #The :as parameter tells the router what to name the route as (You can then add _path or _url to whatever the :as parameter is).
+  #Also, any time you link directly to an ActiveRecord model (e.g. link_to user.email, user), it will try to turn user into user_path.
+  get '/accounts/', to: 'accounts#index', :as => :user
+  patch '/accounts/edit/', to: 'accounts#update'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
