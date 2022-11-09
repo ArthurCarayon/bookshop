e3135904a295be65919643d5bb8c062edd233652
ARTHURC <arthur.carayon@gmail.com>
2022-11-09 22:14:57 +0100

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # You can define all roles on a single server, or split them:
 
 
-set :app_path, "/var/www/#{fetch(:application)}"
+set :app_path, "/var/www/bookshop"
 set :branch, 'main'
-set :deploy_to, fetch(:app_path)
+set :deploy_to, 'bookshop'
 server '161.35.88.149', user: 'alceste', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
