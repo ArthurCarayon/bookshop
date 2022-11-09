f9982993786cbdf093e089d1bdbf8ddf6dd24335
ARTHURC <arthur.carayon@gmail.com>
2022-11-09 18:11:40 +0100

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ append :linked_files,  'config/credentials/production.key'
 set :branch, fetch(:branch, "main")
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/bookshop/"
