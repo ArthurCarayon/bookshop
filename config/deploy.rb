1d9bfec03a17d8ab484f3a26fd0bf7cf214b1412
ARTHURC <arthur.carayon@gmail.com>
2022-11-09 21:55:17 +0100

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.17.1"
 set :application, "bookshop"
 set :repo_url, "git@github.com:ArthurCarayon/bookshop.git"
 set :deploy_to, "/var/www/#{fetch :application}"
-# append :linked_files,  'config/credentials/production.key'
+append :linked_files,  'config/credentials/production.key'
 set :branch, fetch(:branch, "main")
 
 # Default branch is :master
