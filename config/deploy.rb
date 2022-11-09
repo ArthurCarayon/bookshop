31acb5ca3c1517ab6834d777dedce6cef5607b35
ARTHURC <arthur.carayon@gmail.com>
2022-11-09 20:19:18 +0100

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,9 +40,3 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-
-namespace :deploy do
-    desc "No ActiveRecord override"
-    task :migrate do
-    end
-end
