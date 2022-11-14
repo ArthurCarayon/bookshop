dd783480d407504e19192cc9c62ec8857c711139
ARTHURC <arthur.carayon@gmail.com>
2022-11-14 22:48:03 +0100

    git ignore

--- push.sh
+++ push.sh
@@ -2,9 +2,9 @@ IFS= read -r -p $"Commit motif? `echo $'\n> '`" input
 echo "$input"
 if [ -z "$input" ]; 
 then
-    MSG="$input"
-else
     MSG="fix"
+else
+    MSG="$input"
 fi
-git add . ; git commit -m $MSG ; git push origin main 
+git add . ; git commit -m "$MSG" ; git push origin main 
 echo "Done 👌"
