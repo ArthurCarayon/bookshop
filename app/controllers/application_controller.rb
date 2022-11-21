e4a12159298d78bac2216c4cccf12becbb69dc90
ARTHURC <arthur.carayon@gmail.com>
2022-11-21 21:25:50 +0100

    I18n first operation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery
     include Pundit
     require 'net/http'
-    before_action :loaddata
+    before_action :loaddata, :set_locale
   
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   
@@ -17,5 +17,24 @@ class ApplicationController < ActionController::Base
       @q = Book.none.ransack(params[:q])
       @categories = Category.all
       @sitecontent = Sitecontent.first
+
+      if params[:q].blank?
+        @q = Book.none.ransack(params[:q])
+      else
+        @q = Book.ransack(params[:q])
+        @q.sorts = 'created_at desc' if @q.sorts.empty?
+        @books = @q.result.page(params[:page]).per(10)
+      end
+    end
+
+    def set_locale
+      params_locale = params[:locale]
+
+      if params_locale.present?
+        I18n.locale = params_locale.to_s.strip.to_sym
+      else 
+        user_lang_browser = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/)[0].to_s.strip.to_sym
+        I18n.locale = I18n.available_locales.include?(user_lang_browser) ? user_lang_browser : I18n.default_locale
+      end
     end
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,6 @@ class PagesController < ApplicationController
 
   def home
     @books = Book.order(created_at: :desc).page(params[:page]).per(10)
-    @categories = Category.all
-    @sitecontent = Sitecontent.first
-
-    if params[:q].blank?
-      @q = Book.none.ransack(params[:q])
-    else
-      @q = Book.ransack(params[:q])
-      @q.sorts = 'created_at desc' if @q.sorts.empty?
-      @books = @q.result.page(params[:page]).per(10)
-    end
   end
 
 end
