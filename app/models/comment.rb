32bf6a916d1f81118c7acf602ba696d04adfb34a
ARTHURC <arthur.carayon@gmail.com>
2022-11-24 23:15:44 +0100

    Improve comments style and validation + translations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :book
   validates :comment, presence: true
   validates :author, presence: true
-  validates :author_email, presence: true
+  validates :author_email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: :bad_email }
   validates :book_id, presence: true
 end
