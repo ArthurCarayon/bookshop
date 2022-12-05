ea12641ae490fe1988c4ae3cdfa8e4c83d06e844
ARTHURC <arthur.carayon@gmail.com>
2022-12-05 18:09:00 +0100

    fix

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,14 @@ module Bookshop
     config.i18n.available_locales = %i[fr en]
     config.i18n.default_locale = :en
 
+    # load environement variable 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
